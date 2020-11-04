6ea607f6e45d99ba4cd750df72bbac72c1d17a45
YAMAKAWA ERINA <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2020-11-04 17:51:41 +0900

    Initial Commit

---
+++ TripPlanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TripPlanner
+//
+//  Created by 山河絵利奈 on 2020/11/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
