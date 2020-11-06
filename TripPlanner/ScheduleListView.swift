aa7eceb88271b079c7921cd3abeb0923f85b2cde
YAMAKAWA ERINA <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2020-11-07 06:29:48 +0900

    sort schedule

--- TripPlanner/ScheduleListView.swift
+++ TripPlanner/ScheduleListView.swift
@@ -13,19 +13,33 @@ struct ScheduleListView: View {
     @State var deleteSchedule: Schedule?
     
     func indexSave(offsets: IndexSet) {
-        self.deleteSchedule = trip.schedule[offsets.first!]
+        let sortedSchedule = trip.schedule.sorted(by: {lschedule, rschedule -> Bool in
+            if lschedule.day == rschedule.day{
+                return lschedule.start < rschedule.start
+            }else{
+                return lschedule.day < rschedule.day
+            }
+        })
+        self.deleteSchedule = sortedSchedule[offsets.first!]
     }
     
     var body: some View {
+        var sortedSchedule = trip.schedule.sorted(by: {lschedule, rschedule -> Bool in
+            if lschedule.day == rschedule.day{
+                return lschedule.start < rschedule.start
+            }else{
+                return lschedule.day < rschedule.day
+            }
+        })
             List {
-                ForEach(0..<trip.schedule.count, id:\.self) { index in
-                    NavigationLink(destination:ScheduleView(schedule:trip.schedule[index])){
+                ForEach(0..<sortedSchedule.count, id:\.self) { index in
+                    NavigationLink(destination:ScheduleView(schedule:sortedSchedule[index])){
                         HStack{
                             VStack{
-                                Text("\(timeToString(time:trip.schedule[index].start))")
-                                Text("\(timeToString(time:trip.schedule[index].end))")
+                                Text("\(timeToString(time:sortedSchedule[index].start))")
+                                Text("\(timeToString(time:sortedSchedule[index].end))")
                             }
-                            Text("\(trip.schedule[index].title)")
+                            Text("\(sortedSchedule[index].title)")
                         }
                     }
                 }.onDelete(perform: indexSave)
@@ -37,7 +51,13 @@ struct ScheduleListView: View {
                                             realm.delete(schedule)
                                         }
                                         self.deleteSchedule = nil
-                                        print(trip.schedule)
+                                        sortedSchedule = trip.schedule.sorted(by: {lschedule, rschedule -> Bool in
+                                            if lschedule.day == rschedule.day{
+                                                return lschedule.start < rschedule.start
+                                            }else{
+                                                return lschedule.day < rschedule.day
+                                            }
+                                        })
                                     },
                                       secondaryButton: .cancel(Text("キャンセル")) {
                                     }
