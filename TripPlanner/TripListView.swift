17fc841219bbc8977a1bf791a51debbf4e0772f3
YAMAKAWA ERINA <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2020-11-07 09:30:07 +0900

    can delete Trip

--- TripPlanner/TripListView.swift
+++ TripPlanner/TripListView.swift
@@ -10,15 +10,33 @@ import RealmSwift
 
 struct TripListView: View {
     @ObservedObject var tripsObj = Trips()
+    @State var deleteTrip: Trip?
+    
+    func indexSave(offsets: IndexSet) {
+        self.deleteTrip = tripsObj.trips![offsets.first!]
+    }
+    
     var body: some View {
         NavigationView{
             List {
-                ForEach(tripsObj.trips, id: \.id) { trip in
-                    NavigationLink(destination:TripView(trip:trip)){
+                ForEach(0..<tripsObj.trips!.count, id: \.self) { index in
+                    NavigationLink(destination:TripView(trip:tripsObj.trips![index])){
                         VStack {
-                            Text("\(trip.name)")
+                            Text("\(tripsObj.trips![index].name)")
                         }
                     }
+                }.onDelete(perform: indexSave)
+                .alert(item: self.$deleteTrip) { trip in
+                    Alert(title: Text(trip.name + "を削除しますか？"),
+                          primaryButton: .destructive(Text("削除")) {
+                            let realm = try! Realm()
+                            try! realm.write {
+                                realm.delete(trip)
+                            }
+                        },
+                          secondaryButton: .cancel(Text("キャンセル")) {
+                        }
+                    )
                 }
             }.overlay(
                 NavigationLink(destination:TripAddView()){
@@ -37,7 +55,26 @@ struct TripListView: View {
 }
 
 class Trips: ObservableObject {
-    @Published var trips: [Trip] = (try? Realm().objects(Trip.self).map { $0 }) ?? []
+    @Published var trips = (try? Realm().objects(Trip.self))
+        private var notificationTokens: [NotificationToken] = []
+
+        init() {
+            // DBに変更があったタイミングでtripsの変数に値を入れ直す
+            notificationTokens.append(trips!.observe { change in
+                switch change {
+                case let .initial(results):
+                    self.trips = results
+                case let .update(results, _, _, _):
+                    self.trips = results
+                case let .error(error):
+                    print(error.localizedDescription)
+                }
+            })
+        }
+
+        deinit {
+            notificationTokens.forEach { $0.invalidate() }
+        }
 }
 
 struct TripListView_Previews: PreviewProvider {
